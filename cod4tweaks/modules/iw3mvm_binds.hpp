5058961f6271e66091c008ef3e1aa60b3a6b2e01
kruumy <joshuampauls@gmail.com>
2023-08-03 17:00:34 -0400

    fixed shift scroll on iw3mvm

--- cod4tweaks/modules/iw3mvm_binds.hpp
+++ cod4tweaks/modules/iw3mvm_binds.hpp
@@ -7,8 +7,10 @@ namespace modules::iw3mvm_binds
 		const static uint32_t switch_K = reinterpret_cast<uint32_t>(game::functions::iw3mvm_OnKeyEvent) + 0xDD;
 		const static uint32_t switch_J = reinterpret_cast<uint32_t>(game::functions::iw3mvm_OnKeyEvent) + 0x5C;
 		const static uint32_t switch_L = reinterpret_cast<uint32_t>(game::functions::iw3mvm_OnKeyEvent) + 0x104;
+		const static uint32_t switch_C8 = reinterpret_cast<uint32_t>(game::functions::iw3mvm_OnKeyEvent) + 0x1DE;
+		const static uint32_t switch_CD = reinterpret_cast<uint32_t>(game::functions::iw3mvm_OnKeyEvent) + 0x14A;
+		const static uint32_t switch_CE = reinterpret_cast<uint32_t>(game::functions::iw3mvm_OnKeyEvent) + 0x1A3;
 		const static uint32_t switch_default = reinterpret_cast<uint32_t>(game::functions::iw3mvm_OnKeyEvent) + 0x1F0;
-
 		if ( game::dvars::mvm_usedefaultbinds && game::dvars::mvm_usedefaultbinds->current.enabled )
 		{
 			if (key == 'j')
@@ -23,6 +25,22 @@ namespace modules::iw3mvm_binds
 			{
 				__asm jmp	switch_L // Clear Nodes
 			}
+		}
+		if (key == 0xC8)
+		{
+			__asm jmp	switch_C8
+		}
+		else if (key == 0xCD)
+		{
+			__asm jmp	switch_CD
+		}
+		else if (key == 0xCE)
+		{
+			__asm jmp	switch_CE
+		}
+		else
+		{
+			__asm jmp	switch_default
 		}
 		__asm jmp	switch_default
 	}
